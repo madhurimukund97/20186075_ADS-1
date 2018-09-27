c0f9332cae75f9554fc2c8a0f72dad23e3605f79
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-09-27 19:15:43 +0530

    "check_style_score -> 1 of 4 passed. style score: 0/1 "

---
+++ module4/4.1 Balanced Paranthesis/Solution.java
@@ -0,0 +1,128 @@
+import java.util.Scanner;
+// import java.util.Stack;
+import java.util.Arrays;
+/**
+ * List of .
+ */
+class Stack {
+    char [] str;
+    int size;
+    /**
+     * Constructs the object.
+     */
+    public Stack() {
+        str = new char[10];
+        size = 0;
+
+    }
+    
+    public void push(final char ch) {
+        if (isfull()) {
+            str = Arrays.copyOf(str, str.length * (2));
+        }
+        str[size++] = ch;
+
+    }
+    
+    public char pop() {
+        return str[--size];
+
+    }
+    
+    public boolean isempty() {
+        return size == 0;
+    }
+    
+    public char peek() {
+        return str[size - 1];
+    }
+    
+    public boolean isfull() {
+        return size == str.length;
+    }
+
+    // public String balanced(final char[] brac) {
+    //     // Stack s = new Stack();
+
+
+    //     for (char i : brac) {
+    //         if (i == '{' || i == '[' || i == '(') {
+    //             push(i);
+    //         } else {
+    //             if (!isempty()) {
+    //                 char top = peek();
+    //                 if ((top == '{' && i == '}')
+    //                     || (top == '[' && i == ']')
+    //                     || (top == '(' && i == ')')) {
+    //                     pop();
+    //                 } else {
+    //                     return"NO";
+    //                 }
+    //             } else {
+    //                 return "NO";
+    //             }
+    //         }
+    //     }
+    //     if (isempty()) {
+    //         return "YES";
+    //     } else {
+    //         return "NO";
+    //     }
+
+    // }
+    public String balanced(char[] expr) {
+		// if (expr.isempty())
+ 	// 		return "YES";
+ 
+ 		// Stack<Character> stack = new Stack<Character>();
+ 	for (int i = 0; i < expr.length; i++) {
+ 		char current = expr[i];
+ 		if (current == '{' || current == '(' || current == '[') {
+ 			push(current);
+ 		}
+ 		if (current == '}' || current == ')' || current == ']') {
+ 			if (isempty()) {
+ 				return "NO";
+ 			}
+ 			char last = peek();
+ 			if (current == '}' && last == '{' || current == ')' && last == '(' || current == ']' && last == '[')
+ 				pop();
+			else 
+ 				return "NO";
+ 		}
+ 	}
+ 	return isempty()?"YES":"NO";
+ 	}	
+
+
+}
+
+/**
+ * Class for solution.
+ */
+public class Solution {
+	/**
+	 * Constructs the object.
+	 */
+	private Solution() {
+		// constructor not used.
+	}
+	/**
+	 * { main function }.
+	 *
+	 * @param      args  The arguments
+	 */
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+        Stack sc = new Stack();
+        int input = scan.nextInt();
+        scan.nextLine();
+        while (scan.hasNext()) {
+            char[] bracket = scan.nextLine().toCharArray();
+            System.out.println(sc.balanced(bracket));
+
+        }
+	}
+
+	
+}
