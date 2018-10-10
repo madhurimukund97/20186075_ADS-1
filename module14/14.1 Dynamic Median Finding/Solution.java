69676058ae34257fe0ed13106ea63b24793abcaf
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-10-10 14:03:36 +0530

    "ads1_m14_a1: 4/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- module14/14.1 Dynamic Median Finding/Solution.java
+++ module14/14.1 Dynamic Median Finding/Solution.java
@@ -1,113 +1,12 @@
 import java.util.Scanner;
 /**
- * Class for heapsort.
+ * Class for solution.
  */
-class Heapsort {
-    /**
-     * Integer arr type.
-     */
-    private int[] arr;
-    /**
-     * Integer type size.
-     */
-    private int size;
+public class Solution {
     /**
      * Constructs the object.
-     * Time complexity is 1.
-     *
-     * @param      num1     { parameter_description }
      */
-    Heapsort(final int num1) {
-        arr = new int[num1];
-        size = 0;
-    }
-    /**
-     * used to insert.
-     * Time complexity is 1.
-     *
-     * @param      item  The item
-     */
-    void insert(final int item) {
-        arr[size++] = item;
-        sort();
-    }
-    /**
-     * heap sort.
-     * Time complexity is 1.
-     *
-     * @param      num   The number
-     * @param      ind   The ind
-     */
-    void heap(final int num, final int ind) {
-        int largest = ind;
-        int left = 2 * ind + 1;
-        int right = 2 * ind + 2;
-        if (left < num && arr[left] > arr[largest]) {
-            largest = left;
-        }
-        if (right < num && arr[right] > arr[largest]) {
-            largest = right;
-        }
-        if (largest != ind) {
-            swapping(ind, largest);
-            heap(num, largest);
-        }
-    }
-    /**
-     * used to sort.
-     * Time complexity is N.
-     */
-    void sort() {
-        for (int i = (size / 2) - 1; i >= 0; i--) {
-            heap(size, i);
-        }
-        for (int i = size - 1; i >= 0; i--) {
-            swapping(0, i);
-            heap(i, 0);
-        }
-    }
-    /**
-     * used to swap.
-     * Time complexity is 1.
-     *
-     * @param      i1    I 1
-     * @param      j1    The j 1
-     */
-    void swapping(final int i1, final int j1) {
-        int temp = arr[i1];
-        arr[i1] = arr[j1];
-        arr[j1] = temp;
-    }
-    /**
-     * used to get item.
-     * Time complexity is 1.
-     *
-     * @param      ind   The ind
-     *
-     * @return     { description_of_the_return_value }
-     */
-    int getitem(final int ind) {
-        return arr[ind];
-    }
-    /**
-     * used to get size.
-     * Time complexity is 1.
-     *
-     * @return     { description_of_the_return_value }
-     */
-    int getsize() {
-        return size;
-    }
-}
-/**
- * Solution class.
- */
-final class Solution {
-    /**
-     * Constructs the object.
-     * Time complexity is 1.
-     */
-    private Solution() {
+    private Solution () {
         // constructor not used.
     }
     /**
@@ -115,21 +14,47 @@ final class Solution {
      *
      * @param      args  The arguments
      */
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
-        int num = Integer.parseInt(s.nextLine());
-        Heapsort hp =  new Heapsort(num);
+        int num = s.nextInt();
+
+        MinPQ<Float> min = new MinPQ<Float>(num);
+        MaxPQ<Float> max = new MaxPQ<Float>(num);
+
+        Float med = 0.0f;
         for (int i = 0; i < num; i++) {
-            hp.insert(Integer.parseInt(s.nextLine()));
-            int sz = hp.getsize();
-            if ((sz) % 2 == 0) {
-                float med = ((float) hp.getitem((sz - 1) / 2)
-                    + (float) hp.getitem(sz / 2)) / 2;
-                System.out.println(med);
+            Float value = s.nextFloat();
+            if (med < value) {
+                min.insert(value);
             } else {
-                float med = (float) hp.getitem((sz - 1) / 2);
+                max.insert(value);
+            }
+
+            if (min.size() - max.size() > 1) {
+                max.insert(min.delMin());
+
+            }
+            if (max.size() - min.size() > 1) {
+                min.insert(max.delMax());
+                
+            }
+
+            if (min.size() == max.size()) {
+                med = (min.min() + max.max()) / 2;
                 System.out.println(med);
             }
+
+            if (max.size() > min.size()) {
+                med = max.max();
+                System.out.println(med);
+            }
+
+            if (min.size() > max.size()) {
+                med = min.min();
+                System.out.println(med);
+            }
+
         }
+
     }
 }
