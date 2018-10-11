7289e6eccf15433b175925fbdacdcca610580112
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-10-11 18:43:23 +0530

    "ads1_m16_a1: 0/3 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- module16/16.1 BST/Solution.java
+++ module16/16.1 BST/Solution.java
@@ -1,45 +1,139 @@
 import java.util.Scanner;
+/**
+ * Class for book details.
+ */
 class BookDetails implements Comparable<BookDetails> {
+	/**
+	 * String bookname.
+	 */
 	private String bookname;
+	/**
+	 * String author name.
+	 */
 	private String authorname;
+	/**
+	 * String price.
+	 */
 	private String price;
+	/**
+	 * Constructs the object.
+	 * Time complexity is 1.
+	 *
+	 * @param      bookname1    The bookname 1
+	 * @param      authorname1  The authorname 1
+	 * @param      price1       The price 1
+	 */
 	BookDetails(String bookname1, String authorname1, String price1) {
 		this.bookname = bookname1;
 		this.authorname = authorname1;
 		this.price = price1;
 	}
+	/**
+	 * getbookname.
+	 * Time complexity is 1.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public String getbookname() {
 		return this.bookname;
 	}
+	/**
+	 * get author name.
+	 * Time complexity is 1.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public String getauthorname() {
 		return this.authorname;
 	}
+	/**
+	 * get price.
+	 * Time complexity is 1.
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public String getprice() {
 		return this.price;
 	}
+	/**
+	 * compareTo.
+	 * Time complexity is 1.
+	 *
+	 * @param      t     { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public int compareTo(BookDetails t) {
  		return this.getbookname().compareTo(t.getbookname());
  	}
 }
+/**
+ * Class for bst.
+ */
 class BST {
+	/**
+	 * Node root.
+	 */
 	private Node root;
+	/**
+	 * Class for node.
+	 */
     private class Node {
+    	/**
+    	 * Bookdetails.
+    	 */
         private BookDetails key;
+        /**
+         * { var_description }.
+         */
         private String val;
+        /**
+         * { item_description }.
+         */
         private Node left, right;
+        /**
+         * { var_description }.
+         */
         private int size;
-
+        /**
+         * Constructs the object.
+         * Time complexity is 1.
+         *
+         * @param      key1  The key 1
+         * @param      val1  The value 1
+         */
         Node(BookDetails key1, String val1) {
             this.key = key1;
             this.val = val1;
         }
     }
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
+     */
     BST() {
         root = null;
     }
+    /**
+     * put.
+     * Time complexity is 
+     *
+     * @param      key   The key
+     * @param      val   The value
+     */
     public void put(BookDetails key, String val) {
 			root = put(root, key, val);
 		}
+	/**
+	 * put.
+	 * Time complexity is
+	 *
+	 * @param      x     { parameter_description }
+	 * @param      key   The key
+	 * @param      val   The value
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public Node put(Node x, BookDetails key, String val) {
  		if (x == null) {
  			return new Node(key, val);
@@ -54,6 +148,14 @@ class BST {
  		}
  		return x;
 		}
+	/**
+	 * get method.
+	 * Time complexity is 
+	 *
+	 * @param      key   The key
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public String get(BookDetails key) {
 		Node x = root;
 		while (x != null) {
@@ -69,12 +171,22 @@ class BST {
 		 	return null;
 		}
 	}
-
-
-
+/**
+ * solution class.
+ */
 public final class Solution {
+	/**
+	 * Constructs the object.
+	 * Time complexity is 1.
+	 */
 	private Solution() {
+		// constructor not used.
 	}
+	/**
+	 * Client program.
+	 *
+	 * @param      args  The arguments
+	 */
 	public static void main(final String[] args) {
 		Scanner s = new Scanner(System.in);
 		BST bst = new BST();
