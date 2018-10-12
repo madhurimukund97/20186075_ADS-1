713d3306ba48474505247aad954ac9c2f6f98966
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-10-12 16:56:14 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- module17/TaxiCabNumbers/MinPQ.java
+++ module17/TaxiCabNumbers/MinPQ.java
@@ -1,121 +1,118 @@
 import java.util.Comparator;
 import java.util.Iterator;
 import java.util.NoSuchElementException;
-
 /**
- *  The {@code MinPQ} class represents a priority queue of generic keys.
- *  It supports the usual <em>insert</em> and <em>delete-the-minimum</em>
- *  operations, along with methods for peeking at the minimum key,
- *  testing if the priority queue is empty, and iterating through
- *  the keys.
- *  <p>
- *  This implementation uses a binary heap.
- *  The <em>insert</em> and <em>delete-the-minimum</em> operations take
- *  logarithmic amortized time.
- *  The <em>min</em>, <em>size</em>, and <em>is-empty</em> operations take constant time.
- *  Construction takes time proportional to the specified capacity or the number of
- *  items used to initialize the data structure.
- *  <p>
- *  For additional documentation, see <a href="https://algs4.cs.princeton.edu/24pq">Section 2.4</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ * Class for minimum pq.
  *
- *  @author Robert Sedgewick
- *  @author Kevin Wayne
- *
- *  @param <Key> the generic type of key on this priority queue
+ * @param      <Key>  The key
  */
 public class MinPQ<Key> implements Iterable<Key> {
-    private Key[] pq;                    // store items at indices 1 to n
-    private int n;                       // number of items on priority queue
-    private Comparator<Key> comparator;  // optional comparator
-
     /**
-     * Initializes an empty priority queue with the given initial capacity.
+     * { var_description }.
+     */
+    private Key[] pq;
+    /**
+     * { item_description }.
+     */// store items at indices 1 to n
+    private int n; 
+    /**
+     * { item_description }.
+     */// number of items on priority queue
+    private Comparator<Key> comparator;
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
      *
-     * @param  initCapacity the initial capacity of this priority queue
+     * @param      initCapacity  The initialize capacity
      */
     public MinPQ(int initCapacity) {
         pq = (Key[]) new Object[initCapacity + 1];
         n = 0;
     }
-
     /**
      * Initializes an empty priority queue.
+     * Time complexity is 1.
      */
     public MinPQ() {
         this(1);
     }
-
     /**
-     * Initializes an empty priority queue with the given initial capacity,
-     * using the given comparator.
+     * Constructs the object.
+     * Time complexity is 1.
      *
-     * @param  initCapacity the initial capacity of this priority queue
-     * @param  comparator the order in which to compare the keys
+     * @param      initCapacity  The initialize capacity
+     * @param      comparator    The comparator
      */
     public MinPQ(int initCapacity, Comparator<Key> comparator) {
         this.comparator = comparator;
         pq = (Key[]) new Object[initCapacity + 1];
         n = 0;
     }
-
     /**
-     * Initializes an empty priority queue using the given comparator.
+     * Constructs the object.
+     * Time complexity is 1.
      *
-     * @param  comparator the order in which to compare the keys
+     * @param      comparator  The comparator
      */
     public MinPQ(Comparator<Key> comparator) {
         this(1, comparator);
     }
 
     /**
-     * Initializes a priority queue from the array of keys.
-     * <p>
-     * Takes time proportional to the number of keys, using sink-based heap construction.
+     * Constructs the object.
+     * Time complexity is N.
      *
-     * @param  keys the array of keys
+     * @param      keys  The keys
      */
     public MinPQ(Key[] keys) {
         n = keys.length;
         pq = (Key[]) new Object[keys.length + 1];
-        for (int i = 0; i < n; i++)
-            pq[i + 1] = keys[i];
-        for (int k = n / 2; k >= 1; k--)
+        for (int i = 0; i < n; i++) {
+            pq[i+1] = keys[i];
+        }
+        for (int k = n/2; k >= 1; k--) {
             sink(k);
+        }
         assert isMinHeap();
     }
 
     /**
-     * Returns true if this priority queue is empty.
+     * Determines if empty.
+     * Time complexity is 1.
      *
-     * @return {@code true} if this priority queue is empty;
-     *         {@code false} otherwise
+     * @return     True if empty, False otherwise.
      */
     public boolean isEmpty() {
         return n == 0;
     }
 
     /**
-     * Returns the number of keys on this priority queue.
+     * size method.
+     * Time complexity is 1.
      *
-     * @return the number of keys on this priority queue
+     * @return     { description_of_the_return_value }.
      */
     public int size() {
         return n;
     }
 
     /**
-     * Returns a smallest key on this priority queue.
+     * minimum.
+     * Time complexity is 1.
      *
-     * @return a smallest key on this priority queue
-     * @throws NoSuchElementException if this priority queue is empty
+     * @return     { description_of_the_return_value }.
      */
     public Key min() {
-        if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
+        if (isEmpty()) throw new
+            NoSuchElementException("Priority queue underflow");
         return pq[1];
     }
-
-    // helper function to double the size of the heap array
+    /**
+     * resize.
+     * Time complexity is N.
+     *
+     * @param      capacity  The capacity
+     */
     private void resize(int capacity) {
         assert capacity > n;
         Key[] temp = (Key[]) new Object[capacity];
@@ -124,122 +121,165 @@ public class MinPQ<Key> implements Iterable<Key> {
         }
         pq = temp;
     }
-
     /**
-     * Adds a new key to this priority queue.
+     * insert.
+     * Time complexity is logN.
      *
-     * @param  x the key to add to this priority queue
+     * @param      x     { parameter_description }
      */
     public void insert(Key x) {
-        // double size of array if necessary
         if (n == pq.length - 1) resize(2 * pq.length);
 
-        // add x, and percolate it up to maintain heap invariant
+        
         pq[++n] = x;
         swim(n);
         assert isMinHeap();
     }
-
     /**
-     * Removes and returns a smallest key on this priority queue.
+     * del minimum.
+     * Time complexity is logN.
      *
-     * @return a smallest key on this priority queue
-     * @throws NoSuchElementException if this priority queue is empty
+     * @return     { description_of_the_return_value }.
      */
     public Key delMin() {
         if (isEmpty()) throw new NoSuchElementException("Priority queue underflow");
         Key min = pq[1];
         exch(1, n--);
         sink(1);
-        pq[n + 1] = null;   // to avoid loiterig and help with garbage collection
+        pq[n+1] = null;
         if ((n > 0) && (n == (pq.length - 1) / 4)) resize(pq.length / 2);
         assert isMinHeap();
         return min;
     }
-
-
-    /***************************************************************************
-     * Helper functions to restore the heap invariant.
-     ***************************************************************************/
-
+    /**
+     * swim method.
+     * Time complexity is logN.
+     *
+     * @param      k     { parameter_description }
+     */
     private void swim(int k) {
-        while (k > 1 && greater(k / 2, k)) {
-            exch(k, k / 2);
-            k = k / 2;
+        while (k > 1 && greater(k/2, k)) {
+            exch(k, k/2);
+            k = k/2;
         }
     }
-
+    /**
+     * sink.
+     * Time complexity is logN.
+     *
+     * @param      k     { parameter_description }.
+     */
     private void sink(int k) {
-        while (2 * k <= n) {
-            int j = 2 * k;
-            if (j < n && greater(j, j + 1)) j++;
+        while (2*k <= n) {
+            int j = 2*k;
+            if (j < n && greater(j, j+1)) j++;
             if (!greater(k, j)) break;
             exch(k, j);
             k = j;
         }
     }
-
-    /***************************************************************************
-     * Helper functions for compares and swaps.
-     ***************************************************************************/
+    /**
+     * greater.
+     * Time complexity is 1.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
     private boolean greater(int i, int j) {
         if (comparator == null) {
             return ((Comparable<Key>) pq[i]).compareTo(pq[j]) > 0;
-        } else {
+        }
+        else {
             return comparator.compare(pq[i], pq[j]) > 0;
         }
     }
-
+    /**
+     * exchange.
+     * Time complexity is 1.
+     *
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     */
     private void exch(int i, int j) {
         Key swap = pq[i];
         pq[i] = pq[j];
         pq[j] = swap;
     }
-
-    // is pq[1..N] a min heap?
+    /**
+     * Determines if minimum heap.
+     * Time complexity is 1.
+     *
+     * @return     True if minimum heap, False otherwise.
+     */
     private boolean isMinHeap() {
         return isMinHeap(1);
     }
-
-    // is subtree of pq[1..n] rooted at k a min heap?
+    /**
+     * Determines if minimum heap.
+     * Time complexity is 1.
+     *
+     * @param      k     { parameter_description }
+     *
+     * @return     True if minimum heap, False otherwise.
+     */
     private boolean isMinHeap(int k) {
         if (k > n) return true;
-        int left = 2 * k;
-        int right = 2 * k + 1;
+        int left = 2*k;
+        int right = 2*k + 1;
         if (left  <= n && greater(k, left))  return false;
         if (right <= n && greater(k, right)) return false;
         return isMinHeap(left) && isMinHeap(right);
     }
 
-
     /**
-     * Returns an iterator that iterates over the keys on this priority queue
-     * in ascending order.
-     * <p>
-     * The iterator doesn't implement {@code remove()} since it's optional.
+     * iterator.
+     * Time complexity is 1.
      *
-     * @return an iterator that iterates over the keys in ascending order
+     * @return     { description_of_the_return_value }
      */
     public Iterator<Key> iterator() {
         return new HeapIterator();
     }
-
+    /**
+     * Class for heap iterator.
+     * Time complexity is 1.
+     */
     private class HeapIterator implements Iterator<Key> {
-        // create a new pq
         private MinPQ<Key> copy;
-
-        // add all items to copy of heap
-        // takes linear time since already in heap order so no keys move
         public HeapIterator() {
-            if (comparator == null) copy = new MinPQ<Key>(size());
-            else                    copy = new MinPQ<Key>(size(), comparator);
-            for (int i = 1; i <= n; i++)
+            if (comparator == null) {
+                copy = new MinPQ<Key>(size());
+            } else {
+                copy = new MinPQ<Key>(size(), comparator);
+            }                   
+            for (int i = 1; i <= n; i++) {
                 copy.insert(pq[i]);
+            }
         }
-
-        public boolean hasNext()  { return !copy.isEmpty();                     }
-        public void remove()      { throw new UnsupportedOperationException();  }
-
+        /**
+         * Determines if it has next.
+         * Time complexity is 1.
+         *
+         * @return     True if has next, False otherwise.
+         */
+        public boolean hasNext() {
+            return !copy.isEmpty();
+        }
+        /**
+         * remove.
+         * Time complexity is 1.
+         */
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+        /**
+         * next.
+         * Time complexity is 1.
+         *
+         * @return     { description_of_the_return_value }
+         */
         public Key next() {
             if (!hasNext()) throw new NoSuchElementException();
             return copy.delMin();
