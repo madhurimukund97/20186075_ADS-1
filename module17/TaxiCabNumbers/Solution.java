c5878b0093b9acf9003a8ca04847481899529fd1
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-10-12 17:00:50 +0530

    "taxi_cab_numbers: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- module17/TaxiCabNumbers/Solution.java
+++ module17/TaxiCabNumbers/Solution.java
@@ -3,42 +3,77 @@ import java.util.Scanner;
  * Class for taxinumber.
  */
 class Taxinumber implements Comparable<Taxinumber> {
-	/**
-	 * Integer num1.
-	 */
-	private int num1;
-	/**
-	 * Integer num2.
-	 */
-	private int num2;
+    /**
+     * Integer num1.
+     */
+    private int num1;
+    /**
+     * Integer num2.
+     */
+    private int num2;
 
-	/**
-	 * Integer sum.
-	 */
-	private int sum;
-	/**
-	 * Constructs the object.
-	 * Time complexity is 1.
-	 */
-	Taxinumber(int num1, int num2) {
-		this.sum = num1 * num1 * num1 + num2 * num2 * num2;
-		this.num1 = num1;
-		this.num2 = num2;
-	}
-	public int getnum1() {
-		return this.num1;
-	}
-	public int getnum2() {
-		return this.num2;
-	}
-	public int getsum() {
-		return this.sum;
-	}
-	public int compareTo(Taxinumber t) {
-        if (this.sum < t.sum) return -1;
-        if (this.sum > t.sum) return +1;
+    /**
+     * Integer sum.
+     */
+    private int sum;
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
+     */
+    Taxinumber(int num1, int num2) {
+        this.sum = num1 * num1 * num1 + num2 * num2 * num2;
+        this.num1 = num1;
+        this.num2 = num2;
+    }
+    /**
+     * get method for num1.
+     * Time complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int getnum1() {
+        return this.num1;
+    }
+    /**
+     * get method for num2.
+     * Time complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int getnum2() {
+        return this.num2;
+    }
+    /**
+     * getsum.
+     * Time complexity is 1.
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int getsum() {
+        return this.sum;
+    }
+    /**
+     * compareTo.
+     * Time complexity is 1.
+     *
+     * @param      t     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(Taxinumber t) {
+        if (this.sum < t.sum) {
+            return -1;
+        } else if (this.sum > t.sum) {
+            return +1;
+        }
         return 0;
     }
+    /**
+     * Returns a string representation of the object.
+     * Time complexity is 1.
+     *
+     * @return     String representation of the object.
+     */
     public String toString() {
         return sum + " = " + num1 + "^3" + " + " + num2 + "^3";
     }
@@ -47,20 +82,20 @@ class Taxinumber implements Comparable<Taxinumber> {
  * Solution class.
  */
 public final class Solution {
-	/**
-	 * Constructs the object.
-	 * Time complexity is 1.
-	 */
-	private Solution() {
-		// constructor not used.
-	}
-	/**
-	 * Client program.
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(final String[] args) {
-		int num = 600;
+    /**
+     * Constructs the object.
+     * Time complexity is 1.
+     */
+    private Solution() {
+        // constructor not used.
+    }
+    /**
+     * Client program.
+     *
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        final int num = 600;
         Scanner s = new Scanner(System.in);
         int n = s.nextInt();
         int m = s.nextInt();
@@ -77,7 +112,7 @@ public final class Solution {
             } else {
                 res = 0;
             }
-            if(res == m - 1) {
+            if (res == m - 1) {
                 n--;
                 if (n == 0) {
                     System.out.println(sn.getsum());
@@ -88,7 +123,6 @@ public final class Solution {
             if (sn.getnum2() < num) {
                 p.insert(new Taxinumber(sn.getnum1(), sn.getnum2() + 1));
             }
-            }
-        }	
-	
+        }
+    }
 }
