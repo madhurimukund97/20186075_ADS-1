fde9c89df88970df00e844e6c4408facef6bd188
madhurimukund97 <madhurimukund97@msitprogram.net>
2018-10-27 12:00:19 +0530

    "m24_a1_1: 0/4 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ads exam4/1/Solution.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+class Solution {
+	public static void main (String[] args) {
+		Scanner s = new Scanner(System.in);
+		SeparateChainingHashST<Integer,String> obj = new SeparateChainingHashST<>();
+		SeparateChainingHashST<Integer,Double> obj1 = new SeparateChainingHashST<>();
+		int number = Integer.parseInt(s.nextLine());
+		for (int i = 0; i < number; i++) {
+			String[] tokens = s.nextLine().split(",");
+			obj.put(Integer.parseInt(tokens[0]), tokens[1]);
+			obj1.put(Integer.parseInt(tokens[0]), Double.parseDouble(tokens[2]));
+
+             
+		}
+		int number1 = Integer.parseInt(s.nextLine());
+		
+		for (int i = 0; i < number1; i++) {
+			String[] tok = s.nextLine().split(" ");
+			if (tok[0].equals("get") && tok[2].equals("1")) {
+				if (obj.get(Integer.parseInt(tok[1])) == null) {
+					System.out.println("Student doesnot exist");
+				} else {
+					System.out.println(Integer.parseInt(tok[1]));
+				}
+			} else if (tok[0].equals("get") && tok[2].equals("2")) {
+				if (obj1.get(Integer.parseInt(tok[1])) == null) {
+					System.out.println("Student doesnot exist");
+				} else {
+					System.out.println(Integer.parseInt(tok[1]));
+				}
+				System.out.println();
+			}
+		}
+			
+	}
+}
+
+	
+
